73f19321b19e9b497379cfadc384ddec932fa41d
Rakesh Ratnam <107831808+raki1513@users.noreply.github.com>
2022-11-09 19:49:16 +0530

    Add files via upload

---
+++ large.js
@@ -0,0 +1,15 @@
+const a = prompt("Enter a: ");
+const b = prompt("Enter b : ");
+const c = prompt("Enter c: ");
+const d = prompt("Enter d: ");
+const e = prompt("Enter e: ");
+     if(a>b && a>c && a>d && a>e)
+        alert(a+" is greatest no.");
+     else if(b>a && b>c && b>d && b>e)
+        alert(b+" is greatest no.");
+     else if(c>a && c>b && c>d && c>e)
+        alert(c+" is greatest no.");
+     else if(d>a && d>b && d>c && d>e)
+        alert(d+" is greatest no.");
+     else
+        alert(e+" is greatest no."); 
